32bd59af4c46c43271d97c43bcec1ec4665eead4
bonetaurelie <bonetaurelie@yahoo.fr>
2016-07-20 17:56:32 +0200

    création du controller

---
+++ src/OC/CoreBundle/Controller/DefaultController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace OC\CoreBundle\Controller;
+
+use OC\CoreBundle\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('OCCoreBundle:Default:index.html.twig');
+    }
+    
+    
+    public function associationAction(){
+        return $this->render('OCCoreBundle:Default:association.html.twig');
+    }
+    
+    public function observationsAction(){
+        return $this->render('OCCoreBundle:Default:observations.html.twig');
+    }
+    
+    public function contactAction(){
+        $form = $this->get('form.factory')->create(ContactType::class);
+
+
+        // Check the method
+        if ($form->isValid())
+        {
+            $contact = $form->getData();
+
+            $message = \Swift_Message::newInstance()
+                ->setContentType('text/html')
+                ->setSubject($contact['subject'])
+                ->setFrom($contact['email'])
+                ->setTo('xxxxx@gmail.com')
+                ->setBody($contact['message']);
+
+            $this->get('mailer')->send($message);
+
+            // Launch the message flash
+            $this->get('session')->setFlash('notice', 'Merci de nous avoir contacté, nous répondrons à vos questions dans les plus brefs délais.');
+        }
+
+        return $this->render('OCCoreBundle:Default:contact.html.twig',
+            array(
+                'form' => $form->createView(),
+            ));
+
+    }
+    
+    public function connexionAction(){
+        return $this->render('OCCoreBundle:Default:connexion.html.twig');
+    }
+
+    public function mentionsAction(){
+        return $this->render('OCCoreBundle:Default:mentions.html.twig');
+    }
+}
+
