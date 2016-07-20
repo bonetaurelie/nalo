d6125300570d83ad6698cffea728cf91c1ea56c8
bonetaurelie <bonetaurelie@yahoo.fr>
2016-07-20 17:58:41 +0200

    création du formulaire de contact

---
+++ src/OC/CoreBundle/Form/ContactType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace OC\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('prenom',TextType::class)
+            ->add('nom', TextType::class)
+            ->add('email',TextType::class)
+            ->add('message',TextareaType::class)
+            ->add('Envoyer',SubmitType::class)
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'OC\CoreBundle\Entity\Contact'
+        ));
+    }
+}
