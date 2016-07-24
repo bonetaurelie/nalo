f9376bb69d1cd87e5c66b05d55c69fac9cd1f9f9
bonetaurelie <bonetaurelie@yahoo.fr>
2016-07-24 13:35:43 +0200

    création formulaire connexion

---
+++ src/OC/CoreBundle/Form/ConnexionType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace OC\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConnexionType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email',EmailType::class)
+            ->add('password',PasswordType::class)
+            ->add('remember',CheckboxType::class)
+            ->add('Connecter',SubmitType::class, array('label' => 'Se connecter'))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'OC\CoreBundle\Entity\Connexion'
+        ));
+    }
+}
