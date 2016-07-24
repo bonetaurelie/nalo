bfa8bf4dae6afff0cb6af717bb8f76112b80fd96
bonetaurelie <bonetaurelie@yahoo.fr>
2016-07-24 07:00:09 +0200

    création entité réinitialisation

---
+++ src/OC/CoreBundle/Entity/Reinitialisation.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace OC\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Reinitialisation
+ *
+ * @ORM\Table(name="reinitialisation")
+ * @ORM\Entity(repositoryClass="OC\CoreBundle\Repository\ReinitialisationRepository")
+ */
+class Reinitialisation
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Reinitialisation
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
+
