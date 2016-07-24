79495264096d5ab878e47030e2fd9ac3ff6b54ba
bonetaurelie <bonetaurelie@yahoo.fr>
2016-07-24 07:25:46 +0200

    création formulaire newmdp

---
+++ src/OC/CoreBundle/Form/NewMdpType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace OC\CoreBundle\Form;
+
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewMdpType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('mdp',PasswordType::class)
+            ->add('newmdp',PasswordType::class)
+            ->add('Valider', SubmitType::class)
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'OC\CoreBundle\Entity\NewMdp'
+        ));
+    }
+}
