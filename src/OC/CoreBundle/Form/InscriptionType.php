707371fd0e7ad6e01300f3773c98ca0f692a28a6
bonetaurelie <bonetaurelie@yahoo.fr>
2016-07-24 13:35:24 +0200

    création formulaire inscription

---
+++ src/OC/CoreBundle/Form/InscriptionType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace OC\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InscriptionType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('prenom',TextType::class)
+            ->add('nom',TextType::class)
+            ->add('email',EmailType::class)
+            ->add('password',PasswordType::class)
+            ->add('confirm',PasswordType::class)
+            ->add('pro',CheckboxType::class)
+            ->add('Envoyer',SubmitType::class)
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'OC\CoreBundle\Entity\Inscription'
+        ));
+    }
+}
