daa7d0a485cc43fb2a2ce4d6b858345f3c2b54f2
nicolas <pin.nicolas@free.fr>
2016-07-26 23:30:23 +0200

    Développement de la page contact
    - Mise en place d'un objet sans Entité pour le formulaire de contact.
    - Ajout des contraintes
    - Modification de l'affichage pour ajouter les messages d'alerte
    
    test envoi e-mail : OK
    
    Reste à faire :
    
    - Améliorer la view pour prendre en compte les messages d'erreur (ils sont affichés mais sans mise en forme)
    - Amélioration du back car trop de IF cumulé et vérifier si la fonction send du service mailer renvoi true ou 1 si tout c'est bien passé

--- src/OC/CoreBundle/Controller/DefaultController.php
+++ src/OC/CoreBundle/Controller/DefaultController.php
@@ -2,9 +2,7 @@
 
 namespace OC\CoreBundle\Controller;
 
-use OC\CoreBundle\Entity\Recherche;
 use OC\CoreBundle\Form\ConnexionType;
-use OC\CoreBundle\Form\ContactType;
 use OC\CoreBundle\Form\InscriptionType;
 use OC\CoreBundle\Form\NewMdpType;
 use OC\CoreBundle\Form\ProfilType;
@@ -12,7 +10,14 @@ use OC\CoreBundle\Form\RechercheType;
 use OC\CoreBundle\Form\ReinitialisationType;
 use OC\CoreBundle\Form\SaisieType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class DefaultController extends Controller
 {
@@ -38,26 +43,91 @@ class DefaultController extends Controller
         return $this->render('OCCoreBundle:Default:detailRecherche.html.twig');
     }
 
-    public function contactAction(){
-        $form = $this->get('form.factory')->create(ContactType::class);
+    public function contactAction(Request $request){
+        $contactDefaultData = array(
+            'prenom'    => '',
+            'nom'       => '',
+            'email'     => '',
+            'message'   => '',
+        );
 
+        $form = $this->createFormBuilder($contactDefaultData)
+            ->add('prenom', TextType::class, array(
+                'constraints' => array(
+                    new NotBlank()
+                )
+            ))
+            ->add('nom', TextType::class, array(
+                'constraints' => array(
+                    new NotBlank()
+                )
+            ))
+            ->add('email', EmailType::class, array(
+                'constraints' => array(
+                    new Email()
+                )
+            ))
+            ->add('message', TextareaType::class, array(
+                'constraints' => array(
+                    new NotBlank()
+                )
+            ))
+            ->add('Envoyer', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
 
         // Check the method
         if ($form->isValid())
         {
-            $contact = $form->getData();
 
-            $message = \Swift_Message::newInstance()
-                ->setContentType('text/html')
-                ->setSubject($contact['subject'])
-                ->setFrom($contact['email'])
-                ->setTo('xxxxx@gmail.com')
-                ->setBody($contact['message']);
+            try{
+                $contact = $form->getData();
 
-            $this->get('mailer')->send($message);
+                $mailMessage =  "Bonjour, \r\n ".$contact['nom']." ".$contact['prenom'].
+                    " vous a laissé un message sur le site NALO \r\n Voici son message: \r\n \r\n".$contact['message'].
+                    " \r\n\r\n vous pour lui répondre directement via son e-mail : ".$contact['message'].
+                    " \r\n\r\n ".
+                    " Cordialement, \r\n l'association NALO";
 
-            // Launch the message flash
-            $this->get('session')->setFlash('notice', 'Merci de nous avoir contacté, nous répondrons à vos questions dans les plus brefs délais.');
+                $message = \Swift_Message::newInstance()
+                    ->setContentType('text/html')
+                    ->setSubject("Site NALO : message via la page contact")
+                    ->setFrom($this->getParameter('robot_email'))
+                    ->setTo($this->getParameter('contact_email'))
+                    ->setBody($mailMessage);
+
+                $rslt = $this->get('mailer')->send($message);
+
+                //si le service d'envoi d'e-mail retourne autre chose que 1 ou true
+                if($rslt != 1){//@todo il faut que je vérifie ce qu'il retourne exactement
+                    $flashMessage  = "Une erreur est intervenue, si l'erreur persiste veuillez contacter l'administrateur du site.";
+
+                    //on teste l'environnement si on est en test ou en dev on affiche l'erreur à l'erreur
+                    if(in_array($this->get('kernel')->getEnvironment(), array('test','dev'))){
+                        $flashMessage  .= "\r\n".$rslt;
+                    }
+
+                    // Launch the message flash
+                    $this->addFlash('danger', $flashMessage);
+                }
+                else{
+                    // Launch the message flash
+                    $this->addFlash('notice', 'Merci de nous avoir contacté, nous répondrons à vos questions dans les plus brefs délais.');
+                }
+            }
+            catch(\Exception $e){
+                $flashMessage  = "Une erreur est intervenue, si l'erreur persiste veuillez contacter l'administrateur du site.";
+
+                //on teste l'environnement si on est en test ou en dev on affiche l'erreur à l'erreur
+                if(in_array($this->get('kernel')->getEnvironment(), array('test','dev'))){
+                    $flashMessage  .= "\r\n".$e->getMessage();
+                }
+
+                // Launch the message flash
+                $this->addFlash('danger', $flashMessage);
+            }
+
         }
 
         return $this->render('OCCoreBundle:Default:contact.html.twig',
