f4452c2494d9ba1f8ed08ae17bfe595f3f108fc5
nicolas <pin.nicolas@free.fr>
2016-07-27 22:34:31 +0200

    Développement de la page contact
    - Création d'un service pour gérer l'envoie d'e-mail avec un template et qui peut-être réutilisé partout.
    - Création d'un service pour gérer le captcha de Google, mais suppression de celui-ci pour utiliser un bundle EWZ/recaptcha-bundle qui marche super bien
    
    - Modification du nom du controller : DefaultController -> MainController
    - Modification des dossiers etc... en conséquence.
    
    - Ajout dans le dossier view un dossier Email/default.html.twig, qui permettra de personnaliser les e-mails envoyés
    
    - Modification de la vue du contact pour ajouter le captcha et mettre en place les micro-données sur l'adresse de l'asssociation
    
    - modification du layout pour pouvoir ajouter des fichiers JS dans les templates (ajout d'un block autour des JS)
    
    - modification du parametre.yml.dist pour ajouter des nouvelles variables obligatoires

---
+++ src/OC/CoreBundle/Services/MailerTemplating.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace OC\CoreBundle\Services;
+
+use Symfony\Component\Templating\EngineInterface;
+
+/**
+ * Class MailerTemplating
+ *
+ * Cette classe permet d'envoyer des e-mails avec un template twig améliorant le visuel de l'e-mail
+ *
+ * @package OC\CoreBundle\Services
+ * @author NICOLAS PIN <pin.nicolas@free.fr>
+ *
+ */
+class MailerTemplating
+{
+    private $mailer;
+    private $templating;
+
+    /**
+     * MailerTemplating constructor.
+     * @param \Swift_Mailer $mailer le service d'envoi d'email
+     * @param EngineInterface $templating le service de templating
+     */
+    public function __construct(\Swift_Mailer $mailer, EngineInterface $templating)
+    {
+        $this->mailer = $mailer;
+        $this->templating = $templating;
+    }
+
+    /***
+     * Send
+     *
+     * Envoie un e-mail avec un template html
+     *
+     * @param array $bodyMessage le coprs du message
+     * @param string $subject le sujet du message
+     * @param string $from l'e-mail qui envoie
+     * @param string $to l'e-mail qui reçoit
+     * @param string $template le template html
+     * @param string $contentType le type de contenu envoyé
+     * @return int nombre d'e-mail envoyé
+     */
+    public function send(array $bodyContents, $subject, $from, $to, $template, $contentType = 'text/html'){
+        $body = $this->templating->render($template, $bodyContents);
+
+        $message = \Swift_Message::newInstance()
+            ->setContentType('text/html')
+            ->setSubject($subject)
+            ->setFrom($from)
+            ->setTo($to)
+            ->setBody($body);
+
+        return $this->mailer->send($message);
+    }
+}
