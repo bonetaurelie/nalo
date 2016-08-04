07d52fbf4c416371cf758c56e41b0daefcf5d680
nicolas <pin.nicolas@free.fr>
2016-08-04 21:38:04 +0200

    - Ajout de propiétés supplémentaire pour l'entité User - Ajout d'un répository vide pour gérer les utilisateurs par la suite - Ajout de la vue pour la page de connexion lié à fosuserbundle
    - Modification du type de connexion pour qu'un utilisateur puisse se connecter aussi bien avec son username que son email dans notre cas nous utiliserons l'e-mail

--- src/OC/UserBundle/Entity/User.php
+++ src/OC/UserBundle/Entity/User.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @package OC\UserBundle\Entity
  * @ORM\Entity()
  * @ORM\Table(name="nalo_user")
+ * @ORM\Entity(repositoryClass="OC\UserBundle\Repository\UserRepository")
  */
 class User extends BaseUser
 {
@@ -20,8 +21,126 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false, name="first_name")
+     */
+    protected $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false, name="last_name")
+     */
+    protected $lastName;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=false, name="create_at")
+     */
+    protected $createAt;
+
+    /**
+     * @ORM\Column(type="string", length=32, nullable=true, name="key")
+     */
+    protected $key;
+
     public function __construct()
     {
         parent::__construct();
+
+        $this->createAt = new \DateTime();
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set createAt
+     *
+     * @param \DateTime $createAt
+     *
+     * @return User
+     */
+    public function setCreateAt($createAt)
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createAt
+     *
+     * @return \DateTime
+     */
+    public function getCreateAt()
+    {
+        return $this->createAt;
+    }
+
+    /**
+     * Set key
+     *
+     * @param string $key
+     *
+     * @return User
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    /**
+     * Get key
+     *
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->key;
     }
 }
